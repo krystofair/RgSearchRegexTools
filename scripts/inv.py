8a909aa8ef9fd0642fe4147b439f64d5fc3708bd
Krzysztof Kłapyta <33362868+krystofair@users.noreply.github.com>
2025-08-30 05:13:28 +0200

    readme, some scripts already writen

---
+++ scripts/inv.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import sys
+
+# function or method invoke
+PATTERN = r'^[^def|class]\s*(\w+\.)*{func}\('
+
+def format(funcname=r"\w+"):
+    return PATTERN.format(func=funcname)
+
+def parse(*args):
+    format_names = ['funcname']
+    return format(**{format_names[i]: args[i]
+                     for i in range(len(args)) if args[i] != '-'})
+
+if __name__ == "__main__":
+    try:
+        sys.stdout.write(parse(*sys.argv[1:]))
+    except:
+        sys.exit(-1)
+    sys.exit(0)
---
+++ scripts/reimp.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import sys
+
+PATTERN = r'(?:from\s+\.*({module})(\.\w+)?\s+import\s+({func})|import\s+{module}\.?{func})'
+
+def format(modname=r"[\w\.]+", funcname=r"\w*"):
+    return PATTERN.format(module=modname, func=funcname)
+
+def parse(*args):
+    format_names = ['modname', 'funcname']
+    return format(**{format_names[i]: args[i]
+                     for i in range(len(args)) if args[i] != '-'})
+
+if __name__ == "__main__":
+    try:
+        sys.stdout.write(parse(*sys.argv[1:]))
+    except:
+        sys.exit(-1)
+    sys.exit(0)
